e37b9d1a1823e7074360739cad559fa7587fcf8e
Hari Prasath <43212406+vhariprasath@users.noreply.github.com>
2020-04-12 23:23:56 +0530

    Add files via upload

---
+++ addhandler.py
@@ -0,0 +1,12 @@
+import tornado.web
+import book
+import json
+class AddHandler(tornado.web.RequestHandler):
+    def initialize(self, books):
+        self.books = books
+    def get(self):
+        title = self.get_argument('title')
+        author = self.get_argument('author')
+        url = self.get_argument('url')
+        result = self.books.add_book(title, author,url)
+        self.write(result)
